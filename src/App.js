f1a2be6b10d3c544c9bddd730a8b09115f88f897
Johnatan Illi <johnatan.illi@exoscale.ch>
2025-09-18 08:50:31 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hello World
+          Tcheu C't'equipe
         </a>
       </header>
     </div>
